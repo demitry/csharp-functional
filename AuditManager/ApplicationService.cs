c86f52f0d21039136722b7d9efb08ade0eb55caa
DESKTOP-FCMOFM8\Dmitry <aurorabitbucket@gmail.com>
2017-03-06 12:53:06 +0200

    Refactor II

---
+++ AuditManager/ApplicationService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AuditManager
+{
+    class ApplicationService
+    {
+        public readonly string _directoryName;
+        public readonly AuditManager _auditManager;
+        public readonly Persister _persister;
+
+        public ApplicationService(string derectoryName)
+        {
+            _directoryName = derectoryName;
+        }
+
+        public void RemoveMentionsAbout(string visitirName)
+        {
+            FileContent [] files = _persister.ReadDirectory(_directoryName);
+            IReadOnlyList<FileAction> actions = _auditManager.RemoveMentionsAbout(visitirName, files);
+            _persister.ApplyChanges(actions);
+        }
+
+        public void AddRecord(string visitorName, DateTime timeOfVisit)
+        {
+            FileInfo fileInfo = new DirectoryInfo(_directoryName)
+                .GetFiles()
+                .OrderByDescending(x => x.LastWriteTime)
+                .First();
+
+            FileContent file = _persister.ReadFile(fileInfo.Name);
+            FileAction action = _auditManager.AddRecord(file, visitorName, timeOfVisit);
+            _persister.ApplyChanges(new [] {action});
+        }
+    }
+}
+
---
+++ AuditManager/Persister.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AuditManager
+{
+    public class Persister
+    {
+        public FileContent ReadFile(string fileName)
+        {
+            return  new FileContent(fileName, File.ReadAllLines(fileName));
+        }
+
+        public FileContent[] ReadDirectory(string directoryName)
+        {
+            return Directory.GetFiles(directoryName).Select(ReadFile).ToArray();
+        }
+
+        public void ApplyChanges(IReadOnlyList<FileAction> actions)
+        {
+            foreach (var action in actions)
+            {
+                switch (action.Type)
+                {
+                    case ActionType.Delete:
+                    case ActionType.Create:
+                    {
+                        File.WriteAllLines(action.FileName, action.Content);
+                            continue;
+                    }    
+                    case ActionType.Update:
+                    {
+                        File.Delete(action.FileName);
+                            continue;
+                    }
+                    default:
+                        throw new InvalidOperationException();
+                }
+                
+            }
+        }
+    }
+}
